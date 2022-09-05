70094f424dbac19869430d6ca13492c5ca3528e4
박성진 <parkseongjin@bagseongjin-ui-MacBookAir.local>
2022-09-05 21:53:17 +0900

    first homework

--- study/src/findminmax.java
+++ study/src/findminmax.java
@@ -11,7 +11,7 @@ public class findminmax {
                 min = num[i+1];
             }
         }
-        System.out.println("Max: " + max);
-        System.out.println("Min: " + min);
+        System.out.println("Max:" + max);
+        System.out.println("Min:" + min);
     }
 }
