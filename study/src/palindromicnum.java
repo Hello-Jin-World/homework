d22f5e01186938375b673eef10e29ce277e6f360
박성진 <parkseongjin@bagseongjin-ui-MacBookAir.local>
2022-09-07 19:26:26 +0900

    new homework

--- study/src/palindromicnum.java
+++ study/src/palindromicnum.java
@@ -14,8 +14,9 @@ public class palindromicnum {
         else {
             for (int i = 0; i < len / 2; i++) {
                 boolean result = ((int) (num / Math.pow(10, len - i - 1)) % 10 == (int) (num / Math.pow(10, i)) % 10);
+                // 숫자의 대칭을 확인 (맨 앞자리 수와 맨 뒷자리 수와 비교, 앞에서 두 번째 수와 뒤에서 두 번째 수와 비교, 이걸 중간에 끊길 때까지 반복)
                 if (result == false) {
-                    check = 1;
+                    check = 1; // 만약 하나라도 대칭이 아니면 check에 1 입력
                 }
             }
             if (check == 0) {
