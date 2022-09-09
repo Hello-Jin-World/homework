0cf20439d7bce02662acb23ab8a06b9a68471787
박성진 <parkseongjin@192.168.nate.com>
2022-09-09 20:09:51 +0900

    modified missnum

--- study/src/findmissnum.java
+++ study/src/findmissnum.java
@@ -1,20 +1,18 @@
 public class findmissnum {
     public static void main(String[] args){
-        float miss = 0.1f;
-        int[] num = {-2, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int x = num[0];
-        for (int i = 1 ; i < num.length + 1 ; i++){
-            if (x != num[i-1]){
-                miss = x;
-                x = x+1;
+        boolean miss = true;
+        int x = 0;
+        int[] num = {-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int i = num[0] ; i < num[num.length-1] + 1 ; i++){
+            if (i != num[x]){
+                miss = false;
+                System.out.println(i);
+                break;
             }
-            x = x+1;
+            x++;
         }
-        if (miss == 0.1f){
-            System.out.println("Nothing is missing");
-        }
-        else {
-            System.out.println((int) miss);
+        if (miss == true) {
+            System.out.println("Nothing is missing.");
         }
     }
 }
