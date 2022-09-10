0ee328155c71f610c6464a991be7038d3f5e584f
박성진 <parkseongjin@192.168.nate.com>
2022-09-10 17:25:51 +0900

    final homework

--- study/src/findmissnum.java
+++ study/src/findmissnum.java
@@ -1,12 +1,12 @@
 public class findmissnum {
     public static void main(String[] args){
         boolean miss = true;
-        int[] num = {255, 256, 257, 258};
+        int[] num = {255, 257, 258};
         int x = num[0];
         for (int i = 0 ; i < num.length; i++){
             if (x != num[i]){
                 miss = false;
-                System.out.println("The excluded number is " + i + ".");
+                System.out.println("The excluded number is " + x + ".");
                 break;
             }
             x++;
