628568204b5bf67307174e7e7bd2fdbba73bb549
박성진 <parkseongjin@bagseongjin-ui-MacBookAir.local>
2022-09-13 00:06:27 +0900

    new homework

--- study/src/backwards_array.java
+++ study/src/backwards_array.java
@@ -3,17 +3,22 @@ import java.util.Spliterator;
 
 public class backwards_array {
     public static void main(String[] args){
+        char temp = 0;
         System.out.print("Enter array size : ");
         Scanner sc_size = new Scanner(System.in);
         int size = sc_size.nextInt();
-        char[] arr = new char[size]; // 여기까지 배열 사이즈 입력
+        char[] arr = new char[size];// 여기까지 배열 사이즈 입력
         System.out.print("Enter number : ");
         Scanner sc_num = new Scanner(System.in);
-        String num = sc_num.nextLine(); // 숫자를 문자열로 받음
+        arr = sc_num.nextLine().toCharArray(); // 숫자를 배열로 받음
         System.out.print("Result : ");
-        for (int i = 0 ; i < size ; i++){
-            arr[i] = num.charAt(size - i - 1);
-            System.out.print(arr[i]);
+        for (int i = 0 ; i < size / 2 ; i++){
+            temp = arr[size - i - 1];
+            arr[size - i - 1] = arr[i];
+            arr[i] = temp; // 거꾸로 바꿔주는 로직
+        }
+        for (char i : arr){
+            System.out.print(i);
         }
     }
 }
